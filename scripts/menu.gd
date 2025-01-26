e02712578c91dba2afce41b3f6ac37bdc54c3dc2
Osvaldo Ramirez <osvaldo97@live.com.ar>
2025-01-26 10:12:19 -0300

    Menu y animaciones

---
+++ scripts/menu.gd
@@ -0,0 +1,28 @@
+extends Control
+@onready var music_player = $Music
+@onready var pressSound = $PressButton
+@onready var timer = Timer.new()
+@onready var playAnimation = $ColorRect/VBoxContainer/Play/AnimationButton
+@onready var quitAnimation = $ColorRect/VBoxContainer/Quit/AnimationButton
+
+func _ready() -> void:
+	var music = load("res://audio/ggj_title_theme.mp3")
+	music_player.stream = music
+	music_player.play()
+	timer.wait_time = .3
+	timer.one_shot = true
+	add_child(timer) 
+	
+func _on_play_pressed() -> void:
+	pressSound.play()
+	playAnimation.play("animation")
+	timer.start()
+	await timer.timeout
+	get_tree().change_scene_to_file("res://scenes/escena_auto.tscn") #reemplazar por la escena del juego
+
+func _on_quit_pressed() -> void:
+	pressSound.play()
+	quitAnimation.play("animation")
+	timer.start()
+	await timer.timeout
+	get_tree().quit()
